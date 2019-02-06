79bc196ed4854e2ee9b390a1f261a26620f06e32
mantey <contactmantey@gmail.com>
2019-02-06 16:04:28 +0000

    change to artisan command

--- src/Console/Commands/TraitMakeCommand.php
+++ src/Console/Commands/TraitMakeCommand.php
@@ -11,7 +11,11 @@ class TraitMakeCommand extends GeneratorCommand
      *
      * @var string
      */
-    protected $name = 'laratrait:trait';
+
+    protected $signature = 'make:trait
+                        {name : The name of the trait}
+                        {--path= : The path for the trait}
+                        {--func= : List of functions (separated by ,) in trait}';
 
     /**
      * The console command description.
@@ -45,7 +49,69 @@ class TraitMakeCommand extends GeneratorCommand
      */
     protected function getDefaultNamespace($rootNamespace)
     {
-        return $rootNamespace.'\Traits';
+        $pathName = rtrim($this->option('path'), '/');
+        return $rootNamespace.( $pathName ? "\\".$pathName : "" ).'\Traits';
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return bool|null
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function handle()
+    {
+        $name = $this->qualifyClass( trim($this->argument('name')) );
+        $path = $this->getPath($name);
+
+        $functionName = $this->option('func');
+//        if ($functionName) {
+//            dd($functionName);
+//        }
+
+        // First we will check to see if the class already exists. If it does, we don't want
+        // to create the class and overwrite the user's code. So, we will bail out so the
+        // code is untouched. Otherwise, we will continue generating this class' files.
+        if ((! $this->hasOption('force') || ! $this->option('force')) && $this->alreadyExists(trim($this->argument('name')))) {
+            $this->error($this->type.' already exists!');
+
+            return false;
+        }
+
+        // Next, we will generate the path to the location where this class' file should get
+        // written. Then, we will build the class and make the proper replacements on the
+        // stub files so that it gets the correctly formatted namespace and class name.
+        $this->makeDirectory($path);
+
+        $this->files->put($path, $this->buildClass($name, $functionName));
+
+        $this->info($this->type.' created successfully.');
+    }
+
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string $name
+     * @param null $functionName
+     * @return string
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    protected function buildClass($name, $functionName=null)
+    {
+        $stub = $this->files->get($this->getStub());
+
+        $stub = $this->replaceFunction($stub, $name, $functionName);
+
+        return $this->replaceNamespace($stub, $name)->replaceClass($stub, $name);
+    }
+
+
+    public function replaceFunction($stub, $name, $functionName)
+    {
+        $stub = str_replace('DummyFunction', $functionName, $stub);
+
+        return $stub;
     }
 
 
